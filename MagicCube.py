b8f86fd94b3517808af08dae0a60ec352670ff89
PensoGlide <eduardo.augusto.santiago@gmail.com>
2021-11-29 20:04:12 +0000

    Added prints for opening message and results

--- MagicCube.py
+++ MagicCube.py
@@ -1,6 +1,31 @@
 import itertools
 import math
 
+################################################################################
+# Opening message
+
+print("""\n\n
+888b     d888                   d8b                .d8888b.           888                     .d8888b.           888                   888          888                    
+8888b   d8888                   Y8P               d88P  Y88b          888                    d88P  Y88b          888                   888          888                    
+88888b.d88888                                     888    888          888                    888    888          888                   888          888                    
+888Y88888P888  8888b.   .d88b.  888  .d8888b      888        888  888 88888b.   .d88b.       888         8888b.  888  .d8888b 888  888 888  8888b.  888888 .d88b.  888d888 
+888 Y888P 888     "88b d88P"88b 888 d88P"         888        888  888 888 "88b d8P  Y8b      888            "88b 888 d88P"    888  888 888     "88b 888   d88""88b 888P"   
+888  Y8P  888 .d888888 888  888 888 888           888    888 888  888 888  888 88888888      888    888 .d888888 888 888      888  888 888 .d888888 888   888  888 888     
+888   "   888 888  888 Y88b 888 888 Y88b.         Y88b  d88P Y88b 888 888 d88P Y8b.          Y88b  d88P 888  888 888 Y88b.    Y88b 888 888 888  888 Y88b. Y88..88P 888     
+888       888 "Y888888  "Y88888 888  "Y8888P       "Y8888P"   "Y88888 88888P"   "Y8888        "Y8888P"  "Y888888 888  "Y8888P  "Y88888 888 "Y888888  "Y888 "Y88P"  888     
+                            888                                                                                                                                            
+                       Y8b d88P                                                                                                                                            
+                        "Y88P"                                                                                                                                             
+""")
+
+print('\n Welcome to the simplest magic cube calculator! (Hope this crazy title didn\'t get deformatted)')
+print('All you have to do is simply write the amount of numbers in each side of the cube. This script will return each slice of the cube for you!')
+print('Enjoy!')
+
+
+################################################################################
+# User input
+
 while True:
     n = input('\nEnter the amount of numbers per side of the cube: ')
 
@@ -59,7 +84,22 @@ for permutation in permutations:
 ################################################################################
 # Printing Results
  
-#print(q.keys())
-print(q[(1, 1, 1)], q[(1, 2, 1)], q[(1, 3, 1)])
-print(q[(2, 1, 1)], q[(2, 2, 1)], q[(2, 3, 1)])
-print(q[(3, 1, 1)], q[(3, 2, 1)], q[(3, 3, 1)])
+for k in range(1,n+1):
+    print('\nLayer ' + str(k))
+    for i in range(1, n+1):
+
+        line = ''
+
+        for j in range(1, n+1):
+
+            value = str(q[(i, j, k)])
+
+            # To make the values equally spaced
+            white_spaces = ''
+            for nr in range(6 - len(value)):
+                white_spaces += ' '
+
+            line += value + white_spaces
+
+        print(line)
+
